d86d1d93523eade39e952561d0cd97a02a06fa7a
rahul22ramesh <rahul22@uw.edu>
2025-06-17 21:56:04 -0700

    Completed step 1

--- html_render.py
+++ html_render.py
@@ -8,11 +8,18 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element:
 
+    tag = "html"
+
     def __init__(self, content=None):
-        pass
+        self._content = [content]
+
 
     def append(self, new_content):
-        pass
+        self._content.append(new_content)
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        out_file.write("<{}>\n".format(self.tag))
+        for item in self._content:
+            out_file.write(item)
+            out_file.write("\n")
+        out_file.write("</{}>\n".format(self.tag))
